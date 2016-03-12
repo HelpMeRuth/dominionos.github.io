a4bc9f237ea74a34ec0f9427ba59d302beb98f27
Jack Boswell <boswelja@outlook.com>
2016-03-12 22:05:33 +1300

    Preloader and presets

--- js/custom.js
+++ js/custom.js
@@ -5,3 +5,10 @@ $(window).scroll(function() {
       $("#navbar").removeClass("z-depth-2");
    }
 });
+// makes sure the whole site is loaded
+jQuery(window).load(function() {
+        // will first fade out the loading animation
+	jQuery("#status").fadeOut();
+        // will fade out the whole DIV that covers the website.
+	jQuery("#preloader").delay(1000).fadeOut("slow");
+})
