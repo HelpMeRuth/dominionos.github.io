927e8ad951a4413156d8207fead018a76d4933bd
Jack Boswell <boswelja@outlook.com>
2016-03-12 23:11:31 +1300

    Social Feed

--- js/custom.js
+++ js/custom.js
@@ -12,3 +12,17 @@ jQuery(window).load(function() {
         // will fade out the whole DIV that covers the website.
 	jQuery("#preloader").delay(1000).fadeOut("slow");
 })
+$(document).ready(function() {
+  $('.googleplusfeed').kycoGooglePlusFeed2({
+    id: '116899029375914044550'
+    feedPosts: 3,           // Feed posts to show on load
+    postsIncrement: 5,      // Number of feed posts to show on "Show more" button click
+    maxPosts: 10,           // Max number of posts to pull before "Show more" will go to Google+, cannot excced 20 because of Google API in use
+    profileImageSize: 150,  // Max is 250
+    fadeSpeed: 250,         // Fade-in animation duration
+    loadAttachments: true,  // Load images, videos, links and other attachments into feed?
+    orderBy: 'date',        // Either 'date' or 'popularity'
+    sort: 'asc',            // Either 'asc' or 'desc'
+    lang: 'en'              // Default language, can also be set to 'de'
+  });
+});
