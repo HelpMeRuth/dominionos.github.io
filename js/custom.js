cf7650f14841a90bdd61027cce8181273976a51b
Jack Boswell <boswelja@outlook.com>
2016-03-12 23:15:10 +1300

    Tweaks

--- js/custom.js
+++ js/custom.js
@@ -14,10 +14,10 @@ jQuery(window).load(function() {
 })
 $(document).ready(function() {
   $('.googleplusfeed').kycoGooglePlusFeed2({
-    id: '116899029375914044550'
+    id: '108304488984819398570'
     feedPosts: 3,           // Feed posts to show on load
     postsIncrement: 5,      // Number of feed posts to show on "Show more" button click
-    maxPosts: 10,           // Max number of posts to pull before "Show more" will go to Google+, cannot excced 20 because of Google API in use
+    maxPosts: 5,            // Max number of posts to pull before "Show more" will go to Google+, cannot excced 20 because of Google API in use
     profileImageSize: 150,  // Max is 250
     fadeSpeed: 250,         // Fade-in animation duration
     loadAttachments: true,  // Load images, videos, links and other attachments into feed?
